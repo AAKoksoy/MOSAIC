b8e66cd4e896a2185e21961a35129a06e90fec37
Ayse A Koksoy <ayseakoksoy@gmail.com>
2026-07-24 17:54:53 -0500

    Delete mosaic directory

--- mosaic/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-MOSAIC
-Mapping Organizational States Across InterCellular Ecosystems
-
-MOSAIC is a graph-based framework for representing, measuring,
-and interpreting cellular organization in spatial biological data.
-"""
-
-__version__ = "0.1.0"
